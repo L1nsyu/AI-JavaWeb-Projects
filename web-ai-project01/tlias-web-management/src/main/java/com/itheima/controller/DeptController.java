d987818169adfdd9baeba21663fea6c458205c8a
L1nsyu <2860410795@qq.com>
2026-03-03 18:41:33 +0800

    update

--- web-ai-project01/tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ web-ai-project01/tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -5,13 +5,23 @@ import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
 import com.itheima.service.DeptService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class DeptController {
+
+    /*
+     * 如果在项目中使用了Lombok的话,可以直接在类上加入 @Slf4j 这个注解就可以
+     * */
+//    public static final Logger log = LoggerFactory.getLogger(DeptController.class);//固定的
+
 
     @Autowired
     private DeptService deptService;
@@ -23,7 +33,8 @@ public class DeptController {
 
     @GetMapping("/depts")
     public Result list() {
-        System.out.println("查询全部部门数据");
+        //System.out.println("查询全部部门数据");
+        log.info("查询全部部门数据");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
@@ -61,7 +72,9 @@ public class DeptController {
      * */
     @DeleteMapping("/depts")
     public Result delete(Integer id) {
-        System.out.println("根据Id删除部门:" + id);
+        //System.out.println("根据Id删除部门:" + id);
+        //如果有多个参数的情况下,可以使用占位符的方法来写
+        log.info("根据Id删除部门:{}", id);
         deptService.deleteById(id);
         return Result.success();
     }
@@ -72,7 +85,8 @@ public class DeptController {
      * */
     @PostMapping("/depts")
     public Result add(@RequestBody Dept dept) {
-        System.out.println("新增部门：" + dept);
+        //System.out.println("新增部门：" + dept);
+        log.info("新增部门：{}", dept);
         deptService.add(dept);
         return Result.success();
     }
@@ -90,17 +104,19 @@ public class DeptController {
 
     @GetMapping("/depts/{id}")
     public Result getInfo(@PathVariable Integer id) {
-        System.out.println("根据id查询部门:" + id);
+        //System.out.println("根据id查询部门:" + id);
+        log.info("根据id查询部门：{}", id);
         Dept dept = deptService.getById(id);
         return Result.success(dept);
     }
 
     /*
-    * 修改部门
-    * */
+     * 修改部门
+     * */
     @PutMapping("/depts")
     public Result update(@RequestBody Dept dept) {
-        System.out.println("修改部门:" + dept);
+        //System.out.println("修改部门:" + dept);
+        log.info("修改部门：{}", dept);
         deptService.update(dept);
         return Result.success();
     }
